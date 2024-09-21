cfdd0e7c3b42be1afdd21aa74ebdf3ce3b79b4bd
Ben Mitchell <bjosephmitchell@gmail.com>
2024-09-21 18:05:30 +0100

    Add editlint to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,28 +3,38 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import vitest from "@vitest/eslint-plugin";
 
-import { convertToWarnings } from "./dist/index.js";
+import {
+  convertToWarnings,
+  editlint,
+  ruleEntryToWarning,
+} from "./dist/index.js";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts"] },
-  { ignores: ["dist/", "coverage/"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+export default editlint(
+  [
+    { files: ["**/*.{js,mjs,cjs,ts"] },
+    { ignores: ["dist/", "coverage/"] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+      languageOptions: {
+        parserOptions: {
+          projectService: true,
+          tsconfigRootDir: import.meta.dirname,
+        },
       },
     },
-  },
+    {
+      files: ["**/*.js", "**/*.d.ts"],
+      ...tseslint.configs.disableTypeChecked,
+    },
+    convertToWarnings(vitest.configs.recommended),
+    vitest.configs.env,
+  ],
   {
-    files: ["**/*.js", "**/*.d.ts"],
-    ...tseslint.configs.disableTypeChecked,
-  },
-  convertToWarnings(vitest.configs.recommended),
-  vitest.configs.env,
-];
+    condition: /^.*\/?(prefer|no-unused)/,
+    mutation: ruleEntryToWarning,
+  }
+);
